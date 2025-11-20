fe2bc5a8e1e118128cf2f80d612b707aeda07290
fkuroshiro <bobruskadominik@gmail.com>
2025-11-20 21:01:57 +0100

    Edited TabNavigator to adjust icon size and background color of tab bar.

--- frontend/app/src/navigation/TabNavigator.js
+++ frontend/app/src/navigation/TabNavigator.js
@@ -1,30 +1,63 @@
 // src/navigation/TabNavigator.js
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-import HomeScreen from "../screens/HomeScreen";
+
+import MapScreen from "../screens/MapScreen";
 import MissionsScreen from "../screens/MissionsScreen";
-import LeaderboardScreen from "../screens/LeaderboardScreen";
 import ProfileScreen from "../screens/ProfileScreen";
-import MapScreen from "../screens/MapScreen"; // your existing map screen
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: true,
-        }}
+    <SafeAreaView style={{ flex: 1 }} edges={["top"]} backgroundColor="#252424ff">
+      <NavigationContainer>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarShowLabel: false,
+
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Map") {
+              iconName = focused ? "map" : "map-outline";
+            } else if (route.name === "Missions") {
+              iconName = focused ? "flag" : "flag-outline";
+            } else if (route.name === "Profile") {
+              iconName = focused ? "person" : "person-outline";
+            }
+
+            return <Ionicons name={iconName} size={28} color={color} />;
+          },
+
+          tabBarActiveTintColor: "#8400ffff",
+          tabBarInactiveTintColor: "#ffffff",
+          tabBarStyle: {
+            height: 60,
+            paddingBottom: 8,
+            paddingTop: 8,
+            shadowColor: "#000",
+            borderColor: "#252424ff",
+            shadowOffset: {
+              width: 0,
+              height: 1,
+            },
+            shadowOpacity: 1.0,
+            shadowRadius: 10,
+            elevation: 1,
+            backgroundColor: "#252424ff",
+          },
+        })}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Missions" component={MissionsScreen} />
         <Tab.Screen name="Map" component={MapScreen} />
-        <Tab.Screen name="Leaderboard" component={LeaderboardScreen} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
-    </NavigationContainer>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
