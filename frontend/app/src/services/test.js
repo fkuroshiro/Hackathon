d15fb83126f84d398fd0cbf765388b0ba4ae921f
fkuroshiro <bobruskadominik@gmail.com>
2025-11-21 03:42:06 +0100

    test: Wrote changes to frontend/app/src/services/test.js

--- frontend/app/src/services/test.js
+++ frontend/app/src/services/test.js
@@ -1,9 +1,9 @@
 import API_URL from "../config/api";
 
 export async function testBackend() {
-  console.log("Testing:", `${API_URL}/ping`);
+  console.log("Testing:", `${API_URL}/`);
   try {
-    const res = await fetch(`${API_URL}/ping`);
+    const res = await fetch(`${API_URL}/`);
     console.log("Status:", res.status);
     const data = await res.json();
     console.log("Data:", data);
